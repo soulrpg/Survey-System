3225b78317eda192584ade1e59e2ea279c11f28c
soulrpg <soulrpg@users.noreply.github.com>
2023-01-09 23:07:54 +0100

    Implement registration form in vue

---
+++ survey-client/src/components/RegistrationForm.vue
@@ -0,0 +1,60 @@
+<script>
+async function postData(url, data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data, null, "\t")
+    });
+    return await response.json();
+}
+
+export default {
+    data() {
+        return {
+            userData: {
+                email: '',
+                password: {
+                    first: '',
+                    second: '',
+                },
+            },
+            msg: '',
+            error: '',
+        }
+    },
+    methods: {
+        register() {
+            postData('http://localhost:8000/api/register', this.userData)
+            .then((data) => {
+                this.error = data.errors ?? '';
+                if (data.msg === 'success') {
+                    this.userData.email = '';
+                    this.password.first = '';
+                    this.password.second = '';
+                    this.msg = 'Registration successful. You can login now.'
+                }
+            });
+        },
+    }
+}
+</script>
+
+<template>
+    <h2>Register</h2>
+    <p v-if="msg">{{msg}}</p>
+    <p v-if="error">{{error}}</p>
+    <form @submit.prevent="register" action="/" method="post">
+        <label for="email">Email:</label><br/>
+        <input type="text" id="email" v-model="userData.email"><br/>
+        <label for="password">Password:</label><br/>
+        <input type="password" id="password" v-model="userData.password.first"/><br/>
+        <label for="repeat-password">Repeat password:</label><br/>
+        <input type="password" id="repeat-password" v-model="userData.password.second"/><br/>
+        <input type="submit" value="Register"/>
+    </form>
+</template>
+
+<style></style>
--- survey-client/src/views/HomeView.vue
+++ survey-client/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import RegistrationFrom from "../components/RegistrationForm.vue"
 </script>
 
 <template>
   <main>
     Home view!
+    <RegistrationFrom/>
   </main>
 </template>
